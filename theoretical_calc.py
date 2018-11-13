cf7ab40331b5c1bb3012344362cd68b46e79bec0
treyfortmuller <tfortmuller@mac.com>
2018-11-13 14:28:31 -0800

    added plots for part B

--- theoretical_calc.py
+++ theoretical_calc.py
@@ -4,64 +4,150 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import special # for the complimentary error function
-import models # custom script with theoretical models regarding processing physics
+# import models # custom script with theoretical models regarding processing physics
+
+# # INITIAL ION IMPLANTATION DISTRIBUTION
+# x = np.linspace(0, 4e-5, 100)
+
+# # add units here
+# Np = 4.35e17
+# delta_Rp = 5.5e-6
+# Rp = 2e-5
+
+# plt.figure(1)
+# plt.plot(x, Np * np.exp((-(x-delta_Rp)**2)/(2*delta_Rp**2)))
+
+# plt.title("Initial Ion Implantation Distribution")
+# plt.xlabel("Wafer Depth")
+# plt.ylabel("Ion Implantation Concentration")
 
 
-# initial ion implantation concentration (step 1)
-x = np.linspace(0, 4e-5, 100)
 
-# add units here
-Np = 4.35e17
-delta_Rp = 5.5e-6
-Rp = 2e-5
+# # PHOSPHORUS AFTER PRE-DIFFUSION
+# plt.figure(2) 
+# x = np.linspace(0, 5e-7, 100)
 
-# Np * np.exp(-(x-delta_Rp)**2/(2*delta_Rp**2))
+# No = 1.25e21 # solid solubility limited phosphorous concentration
+# D = 9.207e-14 # diffusion coefficient
+# t = 0.17 # hours
 
-plt.figure(1)
-plt.plot(x, Np * np.exp((-(x-delta_Rp)**2)/(2*delta_Rp**2)))
+# C = 1.0/(2*np.sqrt(D*t))
 
-plt.title("Initial Ion Implantation Distribution")
-plt.xlabel("Wafer Depth")
-plt.ylabel("Ion Implantation Concentration")
+# plt.plot(x, No*special.erfc(C*x))
 
-# constant source, solid solubility limited phosphorus diffusion
-plt.figure(2) 
-x = np.linspace(0, 5e-7, 100)
+# plt.title("After Pre-diffusion Phosphorous Distribution")
+# plt.xlabel("Wafer Depth")
+# plt.ylabel("Dopant Concentration")
 
+
+
+# # PHOSPHORUS AFTER DRIVE-IN
+# plt.figure(3)
+# x = np.linspace(0, 5e-7, 100)
+
+# Q = 1.765e14 # total dose from the pre-diffusion step above
+# D = 9.207e-14 # diffusion coefficient
+# t = 0.62 # hours, both the wet oxidation and anneal time
+
+# plt.title("After Drive-in Phosphorous Distribution")
+# plt.xlabel("Wafer Depth")
+# plt.ylabel("Dopant Concentration")
+# plt.plot(x, (Q / np.sqrt(np.pi*D*t))*np.exp(-(x / (2*np.sqrt(D*t)))**2))
+
+###########################################################
+
+# AFTER PRE-DIFFUSION PLOT
+plt.figure(4)
+plt.title("AFTER PRE-DIFFUSION")
+x = np.linspace(1e-5, 10e-5, 10000) # centimeters
+
+# the phosphorus distribution
 No = 1.25e21 # solid solubility limited phosphorous concentration
 D = 9.207e-14 # diffusion coefficient
 t = 0.17 # hours
-
 C = 1.0/(2*np.sqrt(D*t))
+phosphorous = No*special.erfc(C*(x-2.638e-5))
+plt.loglog(x, phosphorous)
 
-plt.plot(x, No*special.erfc(C*x))
+# the gate oxide depth
+oxide_depth = 2.638e-5
+plt.axvline(x=oxide_depth)
 
-plt.title("Constant Source Pre-diffusion Distribution")
-plt.xlabel("Wafer Depth")
-plt.ylabel("Diffused Dopant Concentration")
+# the boron distribution after pre-diffusion
+Q = 6e12 # total dose from the pre-diffusion step above
+Dt = 2.379e-13
+Rp = 2e-5
+boron = (Q / np.sqrt(np.pi*Dt))*np.exp(-((x - Rp) / (2*np.sqrt(Dt)))**2)
+plt.loglog(x, boron)
 
-# limited source drive-in distribution
-plt.figure(3)
-x = np.linspace(0, 5e-7, 100)
+# the intersection points of the dopants
+dopant_intersect_x = np.argwhere(np.diff(np.sign(phosphorous - boron))).flatten()
+plt.plot(x[dopant_intersect_x], phosphorous[dopant_intersect_x], 'ro')
 
-Q = 1.765e14 # total dose from the pre-diffusion step above
-D = 9.207e-14 # diffusion coefficient
-t = 0.62 # hours, both the wet oxidation and anneal time
+# print the junction depth
+print("intersection depth after pre-diffusion: " + str(x[dopant_intersect_x]))
 
-plt.title("Limited Source Drive-in Distribution")
-plt.xlabel("Wafer Depth")
-plt.ylabel("Diffused Dopant Concentration")
-
-plt.plot(x, (Q / np.sqrt(np.pi*D*t))*np.exp(-(x / (2*np.sqrt(D*t)))**2))
+# surface concentration of phosphorus
+surface_conc = No*special.erfc(C*(oxide_depth-2.638e-5))
+print("surface concentration is: "+ str(surface_conc))
 
 plt.show()
 
 
 
+# AFTER DRIVE-IN PLOT
+plt.figure(5)
+plt.title("AFTER DRIVE-IN")
+x = np.linspace(1e-5, 10e-5, 10000)
+
+# the phosphorus distribution
+Q = 1.765e14 # total dose from the pre-diffusion step above
+D = 9.207e-14 # diffusion coefficient
+t = 0.79 # hours, both the wet oxidation and anneal time
+phosphorous = (Q / np.sqrt(np.pi*D*t))*np.exp(-((x-3.471e-5) / (2*np.sqrt(D*t)))**2)
+plt.plot(x, phosphorous)
+
+# the gate oxide depth
+oxide_depth = 3.471e-5
+plt.axvline(x=oxide_depth)
+
+# the boron distribution after pre-diffusion
+Q = 6e12 # total dose from the pre-diffusion step above
+Dt = 2.949e-13
+Rp = 2e-5
+boron = (Q / np.sqrt(np.pi*Dt))*np.exp(-((x - Rp) / (2*np.sqrt(Dt)))**2)
+plt.loglog(x, boron)
+
+# the intersection points of the dopants
+dopant_intersect_x = np.argwhere(np.diff(np.sign(phosphorous - boron)))
+plt.plot(x[dopant_intersect_x], phosphorous[dopant_intersect_x], 'ro')
+
+# print the junction depth
+print("intersection depth after drive-in: " + str(x[dopant_intersect_x]))
+
+# surface concentration of phosphorus
+surface_conc = (Q / np.sqrt(np.pi*D*t))*np.exp(-((3.471e-5-3.471e-5) / (2*np.sqrt(D*t)))**2)
+print("surface concentration is: "+ str(surface_conc))
 
 
+plt.show()
 
 
+# add a vertical line somewhere
+# plt.axvline(x=0.22058956)
+
+
+# plot the intersection of two curves f and g.
+# x = np.arange(0, 1000)
+# f = np.arange(0, 1000)
+# g = np.sin(np.arange(0, 10, 0.01) * 2) * 1000
+
+# plt.plot(x, f, '-')
+# plt.plot(x, g, '-')
+
+# idx = np.argwhere(np.diff(np.sign(f - g))).flatten()
+# plt.plot(x[idx], f[idx], 'ro')
+# plt.show()
 
 
 # def diffusivity(temp):
